f52f0468e5b9573c9f2f89a4268f4371849ff2ec
jagveer <jagveersingh.55555@gmail.com>
2022-04-04 09:32:19 +0530

    commit

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -11,15 +11,6 @@ public class TicTacToeGame {
 		return grid;
 	}
 
-	public static char chooseSymbol() {
-		System.out.println("Which character you want to play as, X or O?");
-		return scanner.next().toUpperCase().charAt(0);
-	}
-
-	public static char assignComputerSymbol(char playerSymbol) {
-		return playerSymbol == 'X' ? 'O' : 'X';
-	}
-
 	public static void main(String[] args) {
 		char[] gameGrid = createGrid();
 	}
